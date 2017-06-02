8c15b26ba7825c8fe5e18553849a9496be50e2d1
Ivairpuerari <ivaair@hotmail.com>
2017-06-02 18:34:41 -0300

    Update Codification.java

--- Codification.java
+++ Codification.java
@@ -1,8 +1,21 @@
 public class Codification{
 
+  	maq machine;
+    public void Code(String L[]){
+    		char c;
 
-  
-    public static void Code(Styrng L[]){
-    
+    		machine = new maq();
+    	  	byte states;
+    	  	int t = 00;
+    	  	System.out.println(t); 
+    		for (int i = 0; i < L[0].length(); i++ ) {
+    				 
+    			c = L[0].charAt(i);
+    			states = machine.estados(c);
+
+    			System.out.println("estado de saida :\n" + states);			
+    			//System.out.println(c);
+    		}
+    //	}
     }
 }
